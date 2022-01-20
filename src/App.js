2da0c9cc30a1711fb798a17e73610439f389f349
Abolfazl <68917563+Abolfazl26s@users.noreply.github.com>
2022-01-20 10:28:09 +0330

    12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './Components/Header';
+import FeedBackItem from './Components/FeedBackItem';
 
 const App = () => {
   return (
     <>
       <Header text />
       <div className="container app">
-        <h1>myApp</h1>
+        <FeedBackItem />
       </div>
     </>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,14 @@
 function Header({ text }) {
+  text = 'FeedBack UI';
+
+  const headerStyle = {
+    background: 'rgba(0,0,0,0.3)',
+    color: 'tomato',
+    textShadow: '0 2px 4px rgba(0,0,0,0.5)',
+  };
+
   return (
-    <header>
+    <header style={headerStyle}>
       <div className="container">
         <h2>{text}</h2>
       </div>
