3b885f70662483688738a6a15612d5fa51f301fb
Abolfazl <68917563+Abolfazl26s@users.noreply.github.com>
2022-01-22 08:30:10 +0330

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Header from './Components/Header';
 import FeedBackItem from './Components/FeedBackItem';
+import { useState } from 'react';
 
 const App = () => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, vel perspiciatis nihil laudantium sit quo ad culpa facilis, itaque autem quod consequuntur optio vero velit officiis. Adipisci corrupti suscipit expedita.',
+    },
+    {
+      id: 2,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, vel perspiciatis nihil laudantium sit quo ad culpa facilis, itaque autem quod consequuntur optio vero velit officiis. Adipisci corrupti suscipit expedita.',
+    },
+    {
+      id: 3,
+      rating: 7,
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, vel perspiciatis nihil laudantium sit quo ad culpa facilis, itaque autem quod consequuntur optio vero velit officiis. Adipisci corrupti suscipit expedita.',
+    },
+    {
+      id: 4,
+      rating: 5,
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, vel perspiciatis nihil laudantium sit quo ad culpa facilis, itaque autem quod consequuntur optio vero velit officiis. Adipisci corrupti suscipit expedita.',
+    },
+  ]);
+
   return (
     <>
       <Header text />
       <div className="container app">
-        <FeedBackItem />
+        <FeedBackItem item={feedback} />
       </div>
     </>
   );
